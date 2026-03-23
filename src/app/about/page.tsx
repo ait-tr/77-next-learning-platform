d90f5cffa1d260ef030df50f9a2a4c97c49be243
Alisher Khamidov <a.a.khamidov@yandex.ru>
2026-03-23 13:23:33 +0100

    init

---
+++ src/app/about/page.tsx
@@ -0,0 +1,12 @@
+export default function About() {
+  return <div className="p-2 bg-amber-100 h-dvh">
+    
+    <h2 className="text-indigo-600 ">About</h2>
+    
+    </div>;
+}
+
+// /about -> ABout
+// /about/me -> Developer X
+// /about/us -> Developer X and Developer Y
+// Remix
