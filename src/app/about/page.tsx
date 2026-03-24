4b3618f3f16f8003ad2acc9c1351ea3de565cff0
Alisher Khamidov <a.a.khamidov@yandex.ru>
2026-03-24 13:12:47 +0100

    add client server examples

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,9 +1,9 @@
 export default function About() {
-  return <div className="p-2 bg-amber-100 h-dvh">
-    
-    <h2 className="text-indigo-600 ">About</h2>
-    
-    </div>;
+  return (
+    <div className="p-2 bg-amber-100 h-dvh">
+      <h2 className="text-indigo-600 ">About</h2>
+    </div>
+  );
 }
 
 // /about -> ABout
